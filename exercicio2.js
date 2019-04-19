803eddcda7641921876bbbdfa6abc15b06fef86d
Sarah Rodrigues Moreira <sarahrodriguesmoreira@gmail.com>
2019-04-19 02:25:23 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -1,18 +1,41 @@
 // Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+let img = document.getElementById("slide");
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+let currentIndex = 0;
+const numberImg = 5;
 
-let botaoAnterior = document.getElementByID("#anterior");
-let botaoProximo = document.getElementByID("#proximo");
+const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/';
+const todasAsImagens = [
+  'philae-parts.jpg',
+  'philae-rosetta.jpg',
+  'philae-separation.jpg',
+  'philae-67-picture.jpg',
+  'philae-collecting.jpg'
+];
+
+let botaoAnterior = document.getElementById("anterior");
+let botaoProximo = document.getElementById("proximo");
 
 botaoAnterior.addEventListener("click", function() {
-
+  if(currentIndex > 0){
+    currentIndex --;
+  }
+  else {
+    currentIndex = numberImg-1;
+  }
+  updateImage();
 });
+
+botaoProximo.addEventListener("click", function() {
+  if(currentIndex < (numberImg-1)){
+    currentIndex ++;
+  }
+  else {
+    currentIndex = 0;
+  }
+  updateImage();
+});
+
+function updateImage(){
+  img.src = servidorDasImagens + todasAsImagens[currentIndex];
+}
